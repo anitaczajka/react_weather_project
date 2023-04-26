b19a98f6220cb3fb4e92fccfdd985bc714b23a85
Anita <anita.czajka.91@outlook.com>
2023-04-26 12:54:16 +0200

    added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Anita Czajka and is{" "}
+        <a
+          href="https://github.com/anitaczajka/react_weather_project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }

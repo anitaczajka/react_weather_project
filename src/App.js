9c9178618de56980debce5c9d543a0cf91f8f952
Anita <anita.czajka.91@outlook.com>
2023-04-26 13:14:34 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Anita Czajka and is{" "}
-        <a
-          href="https://github.com/anitaczajka/react_weather_project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Anita Czajka and is{" "}
+          <a
+            href="https://github.com/anitaczajka/react_weather_project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

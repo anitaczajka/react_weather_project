70366204ce7687ab1ff9226b506e1fc7e570ca12
Anita <anita.czajka.91@outlook.com>
2023-04-26 19:47:41 +0200

    using API call to show current temperature

--- src/Weather.js
+++ src/Weather.js
@@ -1,51 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Type a city..."
-              className="form-control"
-              autoFocus="on"
-            />
+  const [ready, setReady] = useState(false);
+  const [temp, setTemp] = useState(null);
+  function handleResponse(response) {
+    console.log(response.data);
+    setTemp(response.data.temperature.current);
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Type a city..."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
+        </form>
+        <h1>New York</h1>
+        <ul>
+          <li>Wednesday 13:23</li>
+          <li>Cloudy</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+              alt="Cloudy"
             />
+            <span className="temperature">{Math.round(temp)}</span>
+            <span className="unit">°C | °F</span>
           </div>
-        </div>
-      </form>
-      <h1>New York</h1>
-      <ul>
-        <li>Wednesday 13:23</li>
-        <li>Cloudy</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-            alt="Cloudy"
-          />
-          <span className="temperature">11</span>
-          <span className="unit">°C | °F</span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 2%</li>
-            <li>humidity: 58%</li>
-            <li>Wind: 11km/h</li>
-          </ul>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 2%</li>
+              <li>humidity: 58%</li>
+              <li>Wind: 11km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "4f5b6ff5t50992bb0a3f47b106ba0o47";
+    let city = "New York";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
